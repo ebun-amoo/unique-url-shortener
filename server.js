5b53ce57d6391a2fdf642b92a3a35b6d06a466c0
Ebunoluwa Amoo <ebunoluwa.amoo@kibo.school>
2026-02-28 00:37:52 +0000

    create and test home route

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send("Welcome to Ebun's Unique URL Shortener!")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
