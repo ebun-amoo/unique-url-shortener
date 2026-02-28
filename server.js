83c1b5cc305a282355198a66ca8ed3571e90f71b
Ebunoluwa Amoo <ebunoluwa.amoo@kibo.school>
2026-02-28 01:12:15 +0000

    display input url and shortened url

--- server.js
+++ server.js
@@ -5,8 +5,25 @@ const PORT = 3000;
 
 app.use(express.json());
 
+const links = {};
+const originalUrl = "originalUrl";
+const alias = 'abc-123';
+
 app.get('/', (req, res) => {
   res.send("Welcome to Ebun's Unique URL Shortener!")
+});
+
+app.post('/shorten', (req, res) => {
+  const {url} = req.body;
+  links[alias] = {
+    originalUrl: url,
+    shortUrl: `http://localhost:${PORT}/${alias}`
+  }
+  res.json({
+    alias: alias,
+    shortUrl: `http://localhost:${PORT}/${alias}`
+  });
+  console.log(links);
 })
 
 app.listen(PORT, () => {
